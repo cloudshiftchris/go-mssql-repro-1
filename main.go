91455c40724018b1fd18e204f6710a844a71001e
Chris Lee <chris@cloudshiftconsulting.com>
2023-01-07 09:35:23 -0800

    Update for Microsoft rev of driver

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"flag"
 	"fmt"
+	"github.com/golang-sql/sqlexp"
 	_ "github.com/microsoft/go-mssqldb"
+	mssql "github.com/microsoft/go-mssqldb"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"net/url"
@@ -55,13 +58,8 @@ func main() {
 		os.Exit(1)
 	}
 
-	tx, err := db.Begin()
-	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to start tx")
-	}
-	defer tx.Commit()
-
 	log.Info().Msg("Executing query")
+
 	/*
 	  Below query doesn't return any records.  It hangs, never returning from tx.Query.
 
@@ -70,8 +68,8 @@ func main() {
 	    - works when TOP 50 is removed
 	    - continues to hang when columns are removed
 	*/
-	rows, err := tx.Query(
-		`SELECT  TOP 50 'Top 50 queries' as Description,
+
+	var query = `SELECT  TOP 50 'Top 50 queries' as Description,
 	                  a.*,
 	                  SUBSTRING(SqlText, (qs.statement_start_offset/2)+1,
 	   	((CASE qs.statement_end_offset
@@ -95,7 +93,11 @@ func main() {
 	            INNER JOIN sys.dm_exec_query_stats qs on qs.plan_handle = a.plan_handle
 	   	  WHERE queryplan.query_plan IS NOT NULL AND DATEDIFF(hour,qs.last_execution_time,GETDATE()) < 12
 	         ORDER BY qs.total_elapsed_time DESC
-	         `)
+	         `
+
+	//rows, err := queryStraightUp(db, query)
+	//rows, err := queryInTransaction(db,query)
+	rows, err := queryWithExperimentalMessageLoopModel(db, query)
 
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed to execute query")
@@ -127,4 +129,53 @@ func printUsage() {
 	flag.PrintDefaults()
 	fmt.Printf("Example: --username user --password pwd --hostname 108.234.456.789 --clario-version 4.5")
 	os.Exit(1)
+}
+
+func queryStraightUp(db *sql.DB, query string) (*sql.Rows, error) {
+	return db.Query(query)
+}
+
+func queryInTransaction(db *sql.DB, query string) (*sql.Rows, error) {
+	tx, err := db.Begin()
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Commit()
+	return tx.Query(query)
+}
+
+func queryWithExperimentalMessageLoopModel(db *sql.DB, query string) (*sql.Rows, error) {
+	ctx := context.Background()
+	retmsg := &sqlexp.ReturnMessage{}
+	rows, err := db.QueryContext(ctx, query, retmsg)
+	results := true
+	for err == nil && results {
+		msg := retmsg.Message(ctx)
+		switch m := msg.(type) {
+		case sqlexp.MsgNotice:
+			log.Info().Msgf("sqlexp.MsgNotice: %v", m.Message)
+
+		case sqlexp.MsgError:
+			switch e := m.Error.(type) {
+			case mssql.Error:
+				log.Info().Msgf("sqlexp.MsgError: %v", e.Message)
+			}
+		case sqlexp.MsgRowsAffected:
+			log.Info().Msgf("%d rows affected", m.Count)
+		case sqlexp.MsgNextResultSet:
+			results = rows.NextResultSet()
+			if err = rows.Err(); err != nil {
+				log.Info().Msgf("sqlexp.MsgNextResultSet error")
+			}
+			if results {
+				log.Info().Msgf("sqlexp.MsgNextResultSet got first result set")
+			}
+		case sqlexp.MsgNext:
+			inresult := rows.Next()
+			for inresult {
+				log.Info().Msgf("sqlexp.MsgNext")
+			}
+		}
+	}
+	return rows, nil
 }
